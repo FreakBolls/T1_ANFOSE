06a4a82411640956dbc80c99d5d850f503170c63
FreakBolls <wal.gustal@gmail.com>
2026-03-30 18:30:56 -0500

    TEST 1

--- src/test/java/T1_ANFOSE/UserRegistrationTest.java
+++ src/test/java/T1_ANFOSE/UserRegistrationTest.java
@@ -1,10 +1,22 @@
 package T1_ANFOSE;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRegistrationTest {
 
+    @Test
+    @DisplayName("Should faul when fields are empty")
+    void shouldFailWhenFieldsAreEmpty() {
+        UserRegistration userRegistration = new UserRegistration();
+        User user = new User("","","",0);
+        String addResult = userRegistration.register(user);
+        assertEquals("Debe completar todos los campos requeridos", addResult);
 
+
+    }
 
 
 }
